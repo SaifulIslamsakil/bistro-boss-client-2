3b35591888707d76bcb4934e67a93925355d8315
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-03-27 13:45:35 +0600

    added to category section in home pages

---
+++ src/Pages/HomePages/Category/Category.jsx
@@ -0,0 +1,53 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import silder1 from "../../../assets/home/slide1.jpg"
+import silder2 from "../../../assets/home/slide2.jpg"
+import silder3 from "../../../assets/home/slide3.jpg"
+import silder4 from "../../../assets/home/slide4.jpg"
+import silder5 from "../../../assets/home/slide5.jpg"
+// import silder6 from "../../../assets/home/slide6.jpg"
+import SectionHadding from "../../../Shred/SectionHadding/SectionHading"
+const Category = () => {
+    return (
+        <div>
+            <SectionHadding
+                subHadding={"From 11:00am to 10:00pm"}
+                hadding={"oder online"}
+            ></SectionHadding>
+            <div className="max-w-5xl p-5 mx-auto ">
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide><img src={silder1} alt="" />
+                        <p className=" text-2xl font-bold -mt-10 text-white text-center">Salads</p>
+                    </SwiperSlide>
+                    <SwiperSlide><img src={silder2} alt="" />
+                        <p className=" text-2xl font-bold -mt-10 text-white text-center">Soups</p>
+                    </SwiperSlide>
+                    <SwiperSlide><img src={silder3} alt="" />
+                        <p className=" text-2xl font-bold -mt-10 text-white text-center">pizzas</p>
+                    </SwiperSlide>
+                    <SwiperSlide><img src={silder4} alt="" />
+                        <p className=" text-2xl font-bold -mt-10 text-white text-center">desserts</p>
+                    </SwiperSlide>
+                    <SwiperSlide><img src={silder5} alt="" />
+                        <p className=" text-2xl font-bold -mt-10 text-white text-center">Salads</p>
+                    </SwiperSlide>
+                    {/* <SwiperSlide><img src={silder6} alt="" /></SwiperSlide> */}
+
+                </Swiper>
+            </div>
+        </div>
+    );
+};
+
+export default Category;
---
+++ src/Shred/SectionHadding/SectionHading.jsx
@@ -0,0 +1,10 @@
+const SectionHading = ({subHadding, hadding}) => {
+    return (
+        <div className=" text-center lg:w-4/12 mx-auto space-y-3 p-5">
+            <h4 className="text-yellow-600">---{subHadding}---</h4>
+            <h2 className=" mx-auto  border-y-4 py-4 px-8 text-3xl font-bold uppercase">{hadding}</h2>
+        </div>
+    );
+};
+
+export default SectionHading;
