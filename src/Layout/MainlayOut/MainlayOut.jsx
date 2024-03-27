6eff949e1520bfe319901000e906f2f27d4d8610
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-03-27 13:18:56 +0600

    added to navbar in home pages

--- src/Layout/MainlayOut/MainlayOut.jsx
+++ src/Layout/MainlayOut/MainlayOut.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../Shred/Footer/Footer";
+import Navbar from "../../Shred/Navbar/Navbar";
 
 const MainlayOut = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
